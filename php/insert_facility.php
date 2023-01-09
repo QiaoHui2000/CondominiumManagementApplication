295a98b1ec0436a6ebe3be4b5c046d2523892471
QiaoHui2000 <101460361+QiaoHui2000@users.noreply.github.com>
2023-01-10 01:17:05 +0800

    Add files via upload

--- php/insert_facility.php
+++ php/insert_facility.php
@@ -11,65 +11,35 @@ $facilityName = $_POST['facilityName'];
 $location = $_POST['location'];
 $capacity = $_POST['capacity'];
 
+$checkQuery = "SELECT * FROM facility WHERE facilityName = '$facilityName'";
+$result = mysqli_query($conn,$checkQuery);
+$resq = "";
 
+if (mysqli_num_rows($result) > 0) {
+    $resp['code'] = "200";
+    $resp["msg"] = "This facility name is existed.";
+}else{
+	
 $Sql_Query = "INSERT INTO facility (facilityName,location,capacity) VALUES ('$facilityName', '$location', '$capacity')";
 
-// an array to display response
- //$response = array();
- 
- /*if($_POST['facilityName'] && $_POST['location'] &&$_POST['capacity']){
-	 
-	 $facilityName = $_POST['facilityName'];
-	 $location = $_POST['location'];
-	 $capacity = $_POST['capacity'];
-	 
-	 $stmt = $conn->prepare("INSERT INTO 'facility' ('facilityName','location','capacity') VALUES (?,?,?)");
-	 $stmt->bind_param("sss",$facilityName,$location,$capacity);
-	 
-	 if($stmt->execute() == TRUE){
-			// if the script is executed successfully we are
-			// passing data to our response object
-			// with a success message.
-			 $response['error'] = false;
-			 $response['message'] = "data insert successfully!";
-		 } else{
-			 // if we get any error we are passing error to our object.
-			 $response['error'] = true;
-			 $response['message'] = "failed\n ".$conn->error;
-		 }
-	 } else{
-		 // this method is called when user
-		 // donot enter sufficient parameters.
-		 $response['error'] = true;
-		 $response['message'] = "Insufficient parameters";
-	 }
-	 // at last we are printing our response which we get.
-	 echo json_encode($response);*/
+$insert = mysqli_query($conn,$Sql_Query);
+    if ($insert) {
+        $resp['code'] = "201";
+        $resp["msg"] = "Facility Recorded!";
+    }else{
+        $resp['code'] = "202";
+        $resp["msg"] = "Someting Went Wrong!";
+    }
 
-if(mysqli_query($conn,$Sql_Query)){
+}
+echo json_encode($resp);
+/*if(mysqli_query($conn,$Sql_Query)){
 	echo 'Data Submit Successfully';
 	}
 	else{
 		echo 'Try Again';
-		}
+		}*/
 mysqli_close($conn);
 
- 
-
-
-//if(isset($_POST['FacilityName'] && isset($_POST['Location']&& isset($_POST['Capacity'])){
-	/*require "conn.php";
-	$FacilityName = $_POST['FacilityName'];
-	$Location = $_POST['Location'];
-	$Capacity = $_POST['Capacity'];
-	
-	$sql = "INSERT INTO facility VALUES(NULL, '$FacilityName', '$Location', '$Capacity');";
-	
-	if(!$conn->query($sql)){
-		echo "failure";
-	}else{
-		echo "success";
-	}
-}*/
 
 ?>
